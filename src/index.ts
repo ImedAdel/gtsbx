90234fde6219d33d59d6117b278b964be30b508e
Imed Adel <aadelimed@gmail.com>
2019-08-05 12:54:31 +0200

    basique

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
+import { prompt } from 'enquirer'
+
+// prompt({
+// 	type: 'input',
+// 	name: 'username',
+// 	message: 'What is your username?',
+// }).then((answer) =>
+// 	prompt({
+// 		type: 'input',
+// 		name: 'username',
+// 		message: async () => await `Are you ${answer.username}`,
+// 	}).then(res => console.log(res))
+// )
+
+const getName = async () =>
+	prompt([
+		{
+			type: 'input',
+			name: 'username',
+			message: 'What is ya name, buddy?',
+		},
+		{
+			type: 'input',
+			name: 'lastName',
+			message: 'What is ya last Name, ahoi?',
+		},
+	])
+;(async () => {
+	const username: { username?: string; lastName?: string } = await getName()
+	return console.log(username.username, username.lastName)
+})()
+
 export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+	if ('development' === process.env.NODE_ENV) {
+		console.log('boop')
+	}
+	return a + b
+}
