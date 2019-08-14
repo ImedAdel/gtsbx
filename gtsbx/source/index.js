508e96988cc94e508ac387f6dd060e0591043079
Imed Adel <aadelimed@gmail.com>
2019-08-14 15:39:12 +0200

    create the basic menu for the cli

--- gtsbx/source/index.js
+++ gtsbx/source/index.js
@@ -4,49 +4,58 @@
 const inquirer = require('inquirer')
 
 const main = async () => {
-	const getNames = await inquirer.prompt([
+	const getInfo = await inquirer.prompt([
 		{
-			type: 'input',
-			name: 'first_name',
-			message: "What's your first name",
+			type: 'list',
+			name: 'projectType',
+			message: 'Choose the type of your project:',
+			choices: ['Starter', 'Plugin', 'Theme'],
 		},
 		{
-			type: 'input',
-			name: 'last_name',
-			message: "What's your last name",
+			type: 'checkbox',
+			name: 'selectedFeatures',
+			message: 'Check the features needed for your project:',
+			choices: [
+				'Yarn Workspaces',
+				'UI Libraries',
+				'CSS Pre-processors',
+				'CSS-in-JS',
+				'Linter / Formatter',
+				'Unit Testing',
+				'E2E Testing',
+			],
 		},
 	])
-	console.table(getNames)
+	console.table(getInfo)
 
-	const makeSureOfNames = await inquirer.prompt([
-		{
-			type: 'confirm',
-			name: 'isRealName',
-			message: `Is ${getNames.first_name} ${getNames.last_name} your real name?`,
-		},
-	])
-	console.table(makeSureOfNames)
+	// const makeSureOfNames = await inquirer.prompt([
+	// 	{
+	// 		type: 'confirm',
+	// 		name: 'isRealName',
+	// 		message: `Is ${getNames.first_name} ${getNames.last_name} your real name?`,
+	// 	},
+	// ])
+	// console.table(makeSureOfNames)
 
-	const hasRealName = {
-		true: async () => await inquirer.prompt([
-			{
-				type: 'input',
-				name: 'mes',
-				message: 'Say "Yup"',
-			},
-		]),
-		false: async () => await inquirer.prompt([
-			{
-				type: 'input',
-				name: 'mes',
-				message: 'Say "nope"',
-			},
-		]),
-	}
+	// const hasRealName = {
+	// 	true: async () => await inquirer.prompt([
+	// 		{
+	// 			type: 'input',
+	// 			name: 'mes',
+	// 			message: 'Say "Yup"',
+	// 		},
+	// 	]),
+	// 	false: async () => await inquirer.prompt([
+	// 		{
+	// 			type: 'input',
+	// 			name: 'mes',
+	// 			message: 'Say "nope"',
+	// 		},
+	// 	]),
+	// }
 
-	const finalAns = await hasRealName[makeSureOfNames.isRealName]()
-	console.table(finalAns)
-
+	// const finalAns = await hasRealName[makeSureOfNames.isRealName]()
+	// console.table(finalAns)
 }
 
 main()
